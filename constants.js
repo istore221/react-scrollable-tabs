e386a4059047192181e70c2ee81e51156d1b18c0
Kalana Thejitha <istore221@gmail.com>
2020-02-20 13:13:11 +0800

    Create constants.js

---
+++ constants.js
@@ -0,0 +1,22 @@
+export const RESPONSIVE_SIZES = {
+  sm: 576,
+  md: 768,
+  lg: 992,
+  xl: 1200
+}
+
+export const RESPONSIVE_BREAKPOINTS = {
+  sm: `(min-width: ${RESPONSIVE_SIZES.sm}px)`,
+  md: `(min-width: ${RESPONSIVE_SIZES.md}px)`,
+  lg: `(min-width: ${RESPONSIVE_SIZES.lg}px)`,
+  xl: `(min-width: ${RESPONSIVE_SIZES.xl}px)`
+};
+
+
+export const THEMES =  {
+
+    main: {
+      primary: '#00344b'
+    }
+
+}
